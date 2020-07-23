1ceb4e3a1bafb044c48b62b975f5656334a61d10
Juan <juanmrodriguez1989@gmail.com>
2020-07-22 22:20:09 -0300

    cambio gede en index

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 from covid import Covid
 from bs4 import BeautifulSoup 
 import requests
+from datetime import date
+
 
 covid = Covid()
 covid.get_data() 
+
+fecha_hoy = date.today()
 
 paises = covid.list_countries()
 
@@ -40,65 +44,110 @@ def index():
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- CSS only -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet" />
-  <title>Covid 19 - Flaskero</title>
-  <style>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Covid 19 - Flaskero</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet" />
+</head>
+<style>
     body {
-      font-family: "Roboto Mono", monospace;
-      font-size: 14px;
-      color: #ccc;
-      background-color: #316191;
+        font-family: "Roboto Mono", monospace;
+        font-size: 14px;
+        color: rgb(0, 0, 0);
+        background-color: #fff;
     }
-    
-    h2{
+
+    h2 {
         font-size: 18px;
+        line-height: 3rem;
     }
-    
+
     span {
         font-weight: 700;
     }
-    
+
     .marg-bot {
         bottom: 1rem;
     }
 
     .center {
-      text-align: center;
-    }
-    .under {
-        text-decoration: underline;  
+        text-align: center;
     }
 
     .row {
-      padding-top: 100px;
+        padding-top: 100px;
     }
-  </style>
-</head>
+
+    .badge-warning {
+        margin: 1rem 0 2rem 0;
+    }
+
+    .row {
+        flex-direction: column;
+    }
+
+    .col-6 {
+        margin: 1rem auto;
+        max-width: 100%;
+    }
+
+    .red {
+        background-color: #dc3545;
+    }
+
+    .green {
+        background-color: #28a745;
+    }
+
+    @media only screen and (min-width: 768px) {
+        .row {
+            flex-direction: row;
+        }
+
+        .col-6 {
+            margin: 0 auto;
+            max-width: 50%;
+        }
+    }
+</style>
 
 <body>
-  <div class="container">
-    <div class="row center">
-      <div class="col-12 marg-bot">
-        <h1 class="center under">Covid flaskero</h1>
-      </div>
-      <div class="col-6">
-        <h2>Confirmados totales en """ + str(pais) + """: <span>""" + str(confirmados) + """</span></h2>
-        <h2>Activos: <span>""" + str(activos) + """</span></h2>
-        <h2>Muertos: <span>""" + str(muertes) + """</span></h2>
-        <h2>Recuperados: <span>""" + str(recuperados) + """</span></h2>
-      </div>
-      <div class="col-6 center">
-        <img src='https://www.muycomputer.com/wp-content/uploads/2020/07/COVID-19.jpg' alt="peso" class="img-fluid" />
-        <p>Infectados en el mundo: </p>
-        <p>""" + str(covid.get_total_active_cases()) + """</p>
-      </div>
+    <div class="container">
+        <div class="row center">
+            <div class="col-12 marg-bot">
+                <h1 class="center">Covid flaskero</h1>
+            </div>
+            <div class="col-6">
+                <h2>Confirmados totales en """ + str(pais) + """: <span class="alert alert-danger">""" + str(confirmados) + """</span></h2>
+                <span class="badge badge-warning">Reporte - """ + str(fecha_hoy) + """</span>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Activos
+                        <span class="badge badge-primary badge-pill">""" + str(activos) + """</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Muertos
+                        <span class="badge badge-primary badge-pill red">""" + str(muertes) + """</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        Recuperados
+                        <span class="badge badge-primary badge-pill green">""" + str(recuperados) + """</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-6 center">
+                <img src='https://www.muycomputer.com/wp-content/uploads/2020/07/COVID-19.jpg' alt="peso"
+                    class="img-fluid" />
+                <span class="badge badge-warning">Infectados en el mundo: </span>
+                <p>""" + str(covid.get_total_active_cases()) + """</p>
+            </div>
+        </div>
     </div>
-  </div>
+    <footer class="center"><span class="badge badge-primary">Proton-team</span>-Julio 2020</footer>
 </body>
 
 </html>
